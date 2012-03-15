a42e282c8975d905f4febfce80e47c267d28f694
Robert Corujo Rodríguez <robertcorujo@gmail.com>
2012-03-15 22:36:56 +0000

    Finished part2, all scenarios for filtering work

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -1,3 +1,5 @@
+
+
 # Add a declarative step here for populating the DB with movies.
 
 Given /the following movies exist/ do |movies_table|
@@ -21,6 +23,21 @@ Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
   assert false, "Unimplmemented"
 end
 
+Then /I should not see any movies/ do 
+  #  ensure that that e1 occurs before e2.
+  #  page.content  is the entire content of the page as a string.
+  Movie.all.each do |mv|
+    step "I should not see \"#{mv.title}\""
+  end  
+end
+
+Then /I should see all of the movies/ do 
+  #  ensure that that e1 occurs before e2.
+  #  page.content  is the entire content of the page as a string.
+  Movie.all.each do |mv|
+    step "I should see \"#{mv.title}\""
+  end  
+end
 # Make it easier to express checking or unchecking several boxes at once
 #  "When I uncheck the following ratings: PG, G, R"
 #  "When I check the following ratings: G"
@@ -29,4 +46,11 @@ When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
   # HINT: use String#split to split up the rating_list, then
   #   iterate over the ratings and reuse the "When I check..." or
   #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(',').each do |rating|
+    if uncheck
+      step "uncheck \"ratings_#{rating}\""
+    else
+      step "check \"ratings_#{rating}\""
+    end
+  end
 end
