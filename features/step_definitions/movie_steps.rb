05c92c9a469b89df063acbd06889a404b46d9fdb
Robert Corujo Rodríguez <robertcorujo@gmail.com>
2012-03-15 20:05:39 +0000

    Background phases passing, part1 completed

--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,10 +2,14 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
+    Movie.create(movie)
+    #m.title = movie['title']
+    #m.rating = movie['rating']
+    #m.release_date = movie['release_date']
+    #m.save()
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
   end
-  assert false, "Unimplmemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
